3c56984149598f4d2a03423e6f31698b02b62b71
expDh <85827407+expDh@users.noreply.github.com>
2025-05-15 15:57:08 +0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,3 @@
-
 import path from 'path';
 import express from 'express';
 import cors from 'cors';
@@ -6,17 +5,20 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Настройка CORS для разрешения запросов с фронтенда
 app.use(cors({
     origin: ['http://localhost:5173', 'https://cadexchange-test-task-node-react-ts-mui-i9un.onrender.com'],
     credentials: true,
 }));
 
+// Разрешение для обработки JSON
 app.use(express.json());
 
-
+// Обслуживание статических файлов React
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'client/dist'))); 
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
+// Обработка POST-запроса на /api/contact
 app.post('/api/contact', (req, res) => {
     const receivedData = req.body;
     console.log('Received from client: ', receivedData);
@@ -27,7 +29,7 @@ app.post('/api/contact', (req, res) => {
     });
 });
 
-
+// Обслуживание всех остальных маршрутов через index.html для React
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
