eda10a8ae1d2a6ba1a328fdd676d93a2176caaed
expDh <85827407+expDh@users.noreply.github.com>
2025-05-15 14:25:13 +0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,14 @@
-
-
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
+
 
 const app = express();
-const PORT = process.env.PORT || 4000;
-
+const PORT = 4000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',  
+    origin: 'http://localhost:5173'
 }));
-
-
 app.use(express.json());
-
 
 app.post('/api/contact', (req, res) => {
     const receivedData = req.body;
@@ -25,15 +19,6 @@ app.post('/api/contact', (req, res) => {
         data: receivedData
     });
 });
-
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
-
 
 app.listen(PORT, () => {
     console.log(`Server started: http://localhost:${PORT}`);
