e8b36a69be75e7de02977bb48507a70f06456e6f
expDh <85827407+expDh@users.noreply.github.com>
2025-05-16 00:20:59 +0300

    Update Contact.tsx

--- client/src/components/Pages/Contact.tsx
+++ client/src/components/Pages/Contact.tsx
@@ -24,13 +24,13 @@ const Contact = () => {
 
     const onSubmit: SubmitHandler<IFormInput> = async (data: any) => {
             try {
-                const response = await fetch('https://cadexchange-test-task-node-react-ts-mui-i9un.onrender.com/api/contact', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                body: JSON.stringify(data),
-                });
+                const response = await fetch(`${import.meta.env.VITE_API_URL}/contact`, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(data),
+            });
 
                 const result = await response.json();
 
