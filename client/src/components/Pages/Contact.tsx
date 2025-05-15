bccba8c195ead22afbc9d0ffa08a236a6d942e77
expDh <85827407+expDh@users.noreply.github.com>
2025-05-15 16:23:08 +0300

    Update Contact.tsx

--- client/src/components/Pages/Contact.tsx
+++ client/src/components/Pages/Contact.tsx
@@ -1,5 +1,6 @@
 import formCss from '../../styles/form.ts';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
