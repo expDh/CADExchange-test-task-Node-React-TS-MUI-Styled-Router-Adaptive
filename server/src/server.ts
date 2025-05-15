4913333dcb3df63cf831a7464053f56b448e4d4e
expDh <85827407+expDh@users.noreply.github.com>
2025-05-15 15:48:07 +0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,21 @@
+
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 
-
 const app = express();
 const PORT = process.env.PORT || 4000;
+
 app.use(cors({
     origin: ['http://localhost:5173', 'https://cadexchange-test-task-node-react-ts-mui-i9un.onrender.com'],
     credentials: true,
 }));
 
-
-
 app.use(express.json());
+
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'client/dist'))); 
 
 app.post('/api/contact', (req, res) => {
     const receivedData = req.body;
@@ -21,6 +25,11 @@ app.post('/api/contact', (req, res) => {
         message: 'Data successfully received!',
         data: receivedData
     });
+});
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
