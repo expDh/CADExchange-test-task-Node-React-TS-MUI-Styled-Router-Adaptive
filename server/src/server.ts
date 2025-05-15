af4f11771f812915a13e5e8beef334feb84b574b
expDh <85827407+expDh@users.noreply.github.com>
2025-05-15 15:03:29 +0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,14 +4,12 @@ import cors from 'cors';
 
 const app = express();
 const PORT = 4000;
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://cadexchange-test-task-node-react-ts-mui.onrender.com/']
+}));
 
-const corsOptions = {
-  origin: ['http://localhost:5173', 'https://cadexchange-test-task-node-react-ts-mui-i9un.onrender.com'], 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'], 
-};
 
-app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.post('/api/contact', (req, res) => {
